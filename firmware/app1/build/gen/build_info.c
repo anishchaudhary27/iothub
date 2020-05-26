3fbf04100b53ae62d36ebd919767e2cfe1b14d05
anishchaudhary27 <anishchaudhary2706@gmail.com>
2020-05-26 22:00:43 +0530

    added build

---
+++ firmware/app1/build/gen/build_info.c
@@ -0,0 +1,4 @@
+/* Auto-generated, do not edit. */
+const char *build_id = "20200526-162758";
+const char *build_timestamp = "2020-05-26T16:27:58Z";
+const char *build_version = "1.0";
---
+++ firmware/app1/build/gen/mg_build_info.c
@@ -0,0 +1,4 @@
+/* Auto-generated, do not edit. */
+const char *mg_build_id = "20200526-162755/2.17.0-gc31a745";
+const char *mg_build_timestamp = "2020-05-26T16:27:55Z";
+const char *mg_build_version = "2.17.0";
