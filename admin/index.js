04a97295f340539232159b6f0f9c789af46052af
anishchaudhary27 <anishchaudhary2706@gmail.com>
2020-06-03 10:14:22 +0530

    fuck kya ho rha hai

--- admin/index.js
+++ admin/index.js
@@ -8,6 +8,13 @@ admin.initializeApp({
 admin.messaging().send({
     notification:{
         title: 'Good Morning!!',
-        body: 'Have a great day'
-    }
+        body: 'Have a great day anish😃'
+    },
+    token:'eCn5icqEqQY:APA91bG2I-bYjQoo2mTeeeE83Q1wUk4YvCXfa9g1mgsQyPesAYMlGUvDiRy0gA44CNwZoIIoL9C_M3sItPgqb1M4j_EOtVE8dlh_GHqOUFkvfliHLxrUOfBKsCX05gDPDEwYzQl1WtmV'
+})
+.then(al=>{
+    console.log(al)
+})
+.catch(err=>{
+    console.log(err)
 })
